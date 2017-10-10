796f7c874478df5c75cdd7ff8bbc1616cc7f219c
M Safdar Ali Khan <msafdaralikhan65@gmail.com>
2017-10-10 22:19:17 +0500

    Push to heroku

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    @comments = Comment.all(cached_votes_total: :desc)
   end
 
   # GET /comments/1
@@ -30,7 +30,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @submission, notice: 'Comment was successfully created.' }
       else
         format.html { render :new }
       end
